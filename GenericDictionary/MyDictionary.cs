143307860989baf9f12d04e8be4ab2d45553612b
YAGİZ SAVRAN <yagizsav@hotmail.com>
2021-12-15 00:12:42 +0300

    Add project files.

---
+++ GenericDictionary/MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDictionary
+{
+    internal class MyDictionary<TKey,TValue>
+    {
+        TKey[] tKeys;
+        TValue[] tValues;
+        public MyDictionary()
+        {
+            tKeys = new TKey[0];
+            tValues = new TValue[0];
+        }
+        
+        public void Add(TKey keyT, TValue valueT)
+        {
+            TKey[] tempKeysArr = tKeys;
+            TValue[] tempValuesArr = tValues;
+
+            tKeys = new TKey[tKeys.Length+1];
+            tValues = new TValue[tValues.Length+1];
+
+            for (int i = 0; i < tempKeysArr.Length; i++)
+            {
+                tKeys[i] = tempKeysArr[i];
+            }
+
+            for (int i = 0; i < tempValuesArr.Length; i++)
+            {
+                tValues[i] = tempValuesArr[i];
+            }
+
+            tKeys[tKeys.Length - 1] = keyT;
+            tValues[tKeys.Length -1] = valueT;
+        }
+
+        public int Length 
+        { 
+            get { return tKeys.Length; } 
+        }
+        public TValue[] Values 
+        { 
+            get { return tValues;  } 
+        }
+        public TKey[] Keys
+        {
+            get { return tKeys; }
+        }
+    }
+}
---
+++ GenericDictionary/Program.cs
@@ -0,0 +1,14 @@
+﻿using GenericDictionary;
+
+MyDictionary<int, string> myDict = new MyDictionary<int, string>();
+
+myDict.Add(1, "Yağız");
+Console.WriteLine("Length: " + myDict.Length);
+
+myDict.Add(2, "Savran");
+Console.WriteLine("Length: " + myDict.Length);
+
+foreach (var values in myDict.Values)
+{
+    Console.WriteLine("Values: "+ values);
+}
